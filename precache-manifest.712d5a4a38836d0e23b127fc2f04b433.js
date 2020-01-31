1498369b43a7ac1ad4afaacffdfe69da2ce7db8a
clearink <1561939638@qq.com>
2020-01-31 21:14:52 +0800

    hahah

---
+++ precache-manifest.712d5a4a38836d0e23b127fc2f04b433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ade027a56ce7b7a973215efff81c65",
+    "url": "/audio-wave/index.html"
+  },
+  {
+    "revision": "e4be3473495ec9e316b3",
+    "url": "/audio-wave/static/css/2.bb425fa0.chunk.css"
+  },
+  {
+    "revision": "d3826d6ca257f02b65bf",
+    "url": "/audio-wave/static/css/main.34d1a948.chunk.css"
+  },
+  {
+    "revision": "e4be3473495ec9e316b3",
+    "url": "/audio-wave/static/js/2.6cf1904d.chunk.js"
+  },
+  {
+    "revision": "d3826d6ca257f02b65bf",
+    "url": "/audio-wave/static/js/main.5474d120.chunk.js"
+  },
+  {
+    "revision": "331ec13f6dbf68fbbe80",
+    "url": "/audio-wave/static/js/runtime-main.64e5a6b2.js"
+  }
+]);
